69474849d2d22403d962e035748a610530d54c31
Md Nur Mohammad <82093868+mmohamm5@users.noreply.github.com>
2025-01-19 08:14:55 +0000

    second session

---
+++ class02.py
@@ -0,0 +1,6 @@
+#  F - string
+name = input ("Enter the name:")
+working_hour = int(input("Enter the working hour: "))
+print (f"{name} working payment is {working_hour * 8}")
+
+#### Upper_case_lower_case
