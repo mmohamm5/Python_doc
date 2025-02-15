bd8c21fbfd42dd1549b10d913f00e0ddbdfea614
Md Nur Mohammad <82093868+mmohamm5@users.noreply.github.com>
2025-02-15 09:15:20 +0000

    updates day 5 code

--- class05.py
+++ class05.py
@@ -15,3 +15,32 @@ print(bool([0, 1, 2, 3]))   # True
 
 print(bool(True))           # True
 print(bool(False))          # False
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(a == b)  # True
+print(a is b)  # False
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(id(a))  # 139806639351360
+print(id(b))  # 139806638418944
+
+print(a == b)  # True
+print(a is b)  # False
+
+age = int(input("How old are you? "))
+
+if age < 18:
+      print("Sorry, we can't serve you.")
+else:
+      print("We ready to serve")
+
+age = int(input("How old are you? "))
+
+if age < 18:
+      print("Sorry, we can't serve you.")
+else:
+    chosen_drink = input("What can I get for you? ")
