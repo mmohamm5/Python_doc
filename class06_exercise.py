8122353cd38739b726643cc0f059ff0bdfed9a1e
Md Nur Mohammad <82093868+mmohamm5@users.noreply.github.com>
2025-02-16 10:07:22 +0000

    updated class06 exercise

--- class06_exercise.py
+++ class06_exercise.py
@@ -7,3 +7,22 @@ employees = [
     ("Charlie Lee", 50, 15.50),
     ("Bob Smith", 20, 7.00)
 ]
+for employee_list in employees:
+        due_amount = employee_list [1] * employee_list[2]
+        print(f"{employee_list[0]} due amount is : {due_amount}")
+
+#2) For the employees above, print out those who are earning an hourly wage above average.
+total_amount = 0
+hourly_total = 0
+for employee_list in employees:
+        hourly_total +=  employee_list[2]
+
+        #print(f"{employee_list[0]} due amount is : {due_amount}")
+        total_amount = total_amount +hourly_total
+
+avarage = hourly_total/len(employees)
+print(avarage)
+
+for employee_list in employees:
+        if employee_list[2] > avarage:
+              print(f"{employee_list[0]} is getting hourly wage above avarage")
